f15ef7943d5ad13fa1a10fb97a25876b35f98276
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-07-22 22:27:01 +0430

    feat: create custom user and profile model

--- apps/authnz/models.py
+++ apps/authnz/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    twitter = models.CharField(max_length=50, null=True, blank=True, unique=True)
+    telegram = models.CharField(max_length=50, null=True, blank=True, unique=True)
+    instagram = models.CharField(max_length=50, null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance=None, created=False, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
