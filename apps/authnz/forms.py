06d17a96eff0a7b14bb07916388d0a3b214e6c0d
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-07-24 03:46:51 +0430

    feat: create a signin form

--- apps/authnz/forms.py
+++ apps/authnz/forms.py
@@ -15,3 +15,8 @@ class SignUpForm(forms.ModelForm):
         help_texts = {
             "username": "نام کاربری باید یکتا باشد",
         }
+
+
+class SignInForm(forms.Form):
+    username = forms.CharField(min_length=3, max_length=150, label="نام کاربری")
+    password = forms.CharField(min_length=8, max_length=150, widget=forms.PasswordInput, label=" رمز عبور")
