2cf750eaf798b947ff1c5b2fca25c46686297d58
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-07-24 03:45:16 +0430

    feat: create signin view

--- apps/authnz/views.py
+++ apps/authnz/views.py
@@ -1,10 +1,33 @@
-from django.views.generic import CreateView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, render
+from django.views.generic import CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
-from .forms import SignUpForm
+from .forms import SignUpForm, SignInForm
+from .models import User
 
 
 class SignUpView(CreateView):
     template_name = "authnz/signup.html"
     form_class = SignUpForm
     success_url = reverse_lazy("authnz:signup")
+
+
+def signin_view(request):
+    if request.method == "POST":
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            passowrd=request.POST["password"]
+        )
+
+        if user is None:
+            return redirect(reverse_lazy("authnz:signin"))
+
+        login(user)
+        return redirect(reverse_lazy("authnz:signout"))
+
+    form = SignInForm()
+    return render(request, "authnz/signin.html", context={"form": form})
