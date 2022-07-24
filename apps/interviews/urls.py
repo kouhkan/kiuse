26d3e05f2094bbddfbd86f3c6b10ee32bb739370
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-07-24 15:38:37 +0430

    feat: add update url

--- apps/interviews/urls.py
+++ apps/interviews/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from apps.interviews.views import CreateInterviewView, UserInterviewView
+from apps.interviews.views import (CreateInterviewView,
+                                   UserInterviewView,
+                                   UpdateInterviewView)
 
 
 app_name = "interviews"
@@ -8,4 +10,5 @@ app_name = "interviews"
 urlpatterns = [
     path("", CreateInterviewView.as_view(), name="create"),
     path("<pk>/", UserInterviewView.as_view(), name="detail"),
+    path("edit/<pk>/", UpdateInterviewView.as_view(), name="update"),
 ]
