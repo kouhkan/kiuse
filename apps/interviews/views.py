09c46603701fd68229b1f9f60ced95e5eaca7c77
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-07-24 15:38:46 +0430

    feat: add interview update view

--- apps/interviews/views.py
+++ apps/interviews/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DetailView
 
-from apps.interviews.forms import CreateInterviewForm
+from apps.interviews.forms import CreateInterviewForm, UpdateInterviewForm
 from apps.interviews.models import Interview
 
 
@@ -21,3 +22,16 @@ class CreateInterviewView(LoginRequiredMixin, CreateView):
         instance = form.save(commit=False)
         setattr(instance, "user", self.request.user)
         return super().form_valid(form)
+
+
+class UpdateInterviewView(LoginRequiredMixin, UpdateView):
+    template_name = "interviews/update.html"
+    model = Interview
+    form_class = UpdateInterviewForm
+    success_url = reverse_lazy("users:index")
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.user != self.request.user:
+            return redirect(reverse_lazy("users:index"))
+        return super().dispatch(request, *args, **kwargs)
