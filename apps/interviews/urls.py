c895dfadba8b732c58e7b8340964d6455a135eea
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-07-24 15:21:40 +0430

    feat: add detail url

--- apps/interviews/urls.py
+++ apps/interviews/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.interviews.views import CreateInterviewView
+from apps.interviews.views import CreateInterviewView, UserInterviewView
 
 
 app_name = "interviews"
 
 urlpatterns = [
     path("", CreateInterviewView.as_view(), name="create"),
+    path("<pk>/", UserInterviewView.as_view(), name="detail"),
 ]
