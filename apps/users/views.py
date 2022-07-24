4ad00e486344cdc1c041ec9848fe1e8f63472af9
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-07-24 15:05:30 +0430

    feat: add user's interview to profile

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from apps.interviews.models import Interview
+
 
 @login_required
 def index(request):
-    return render(request, "users/index.html", context={})
+    interview = Interview.objects.filter(user=request.user).first()
+    return render(request, "users/index.html", context={"interview": interview})
